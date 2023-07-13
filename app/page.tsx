dff57a673127d11ae044dffc4a7d9542e599506f
Victor Oliveira <vggaia10@gmail.com>
2023-07-12 21:40:25 -0300

    chore: iniciando projeto

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+import { Container } from "@/app/styles";
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <input />
+      <button>Avaliar</button>
+    </Container>
+  );
+};
+
+export default Home;
